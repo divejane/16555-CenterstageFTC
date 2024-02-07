663502e99517dea6a054293c6b76de7ffd528b95
divejane <sonny.nichols5689@gmail.com>
2024-02-07 12:36:03 -0600

    adjusted for narrow-FOV cameras

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/DB_blueSideAuto.java
@@ -0,0 +1,156 @@
+package org.firstinspires.ftc.teamcode.test;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.test.pipelines.DB_bPropPL;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+
+@Autonomous
+public class DB_blueSideAuto extends LinearOpMode {
+    OpenCvWebcam webcam;
+    org.firstinspires.ftc.teamcode.test.pipelines.DB_bPropPL bPropPL;
+
+    private DcMotor leftBackMotor, leftFrontMotor, rightBackMotor, rightFrontMotor;
+
+    org.firstinspires.ftc.teamcode.test.pipelines.DB_bPropPL.PropPosition position = null;
+
+    @Override
+    public void runOpMode() {
+        /**
+         * NOTE: Many comments have been omitted from this sample for the
+         * sake of conciseness. If you're just starting out with EasyOpenCv,
+         * you should take a look at {@link InternalCamera1Example} or its
+         * webcam counterpart, {@link WebcamExample} first.
+         */
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        bPropPL = new DB_bPropPL();
+        webcam.setPipeline(bPropPL);
+
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                webcam.startStreaming(640, 480, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                /*
+                 * This will be called if the camera could not be opened
+                 */
+            }
+        });
+
+        leftBackMotor  = hardwareMap.get(DcMotor.class, "leftBackMotor");
+        leftFrontMotor  = hardwareMap.get(DcMotor.class, "leftFrontMotor");
+        rightBackMotor  = hardwareMap.get(DcMotor.class, "rightBackMotor");
+        rightFrontMotor  = hardwareMap.get(DcMotor.class, "rightFrontMotor");
+
+        leftFrontMotor.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontMotor.setDirection(DcMotor.Direction.REVERSE);
+        leftBackMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        DcMotor slideLift       = hardwareMap.dcMotor.get("slideLift");
+
+        telemetry.addLine("Initializing...");
+        
+        while (!isStarted() && !isStopRequested()) {
+            position = bPropPL.returnPos();
+            telemetry.addData("PROP FOUND @", position );
+            telemetry.update();
+            sleep(50);
+        }
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            webcam.stopStreaming();
+
+            slideLift.setTargetPosition(-500);
+            slideLift.setPower(0.75);
+            slideLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            sleep(1000);
+
+            telemetry.addData("Auto running using", position );
+            telemetry.update();
+
+            if (position.toString().equals("LEFT")) {
+                drive(750,750,750,750,0.35);
+                sleep(1000);
+                drive(1200,0,1200,0,0.35);
+                sleep(1000);
+                drive(-1200,0,-1200,0,0.35);
+                sleep(1000);
+                slideLift.setTargetPosition(0);
+                slideLift.setPower(0.75);
+                slideLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                sleep(50000);
+            }
+            if (position.toString().equals("CENTER")) {
+                drive(1400,1400,1400,1400,0.35);
+                sleep(1000);
+                drive(-500,-500,-500,-500,0.35);
+                sleep(1000);
+                slideLift.setTargetPosition(0);
+                slideLift.setPower(0.75);
+                slideLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                sleep(50000);
+            }
+            else {
+                drive(750,750,750,750,0.35);
+                sleep(1000);
+                drive(0,1350,0,1350,0.35);
+                sleep(1000);
+                drive(0,-1350,0,-1350,0.35);
+                sleep(1000);
+                slideLift.setTargetPosition(0);
+                slideLift.setPower(0.75);
+                slideLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                sleep(50000);
+            }
+
+        }
+    }
+    public void drive(int rB, int lB, int rF, int lF, double power) {
+
+        rightBackMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftBackMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFrontMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftFrontMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        rightBackMotor.setTargetPosition(rB);
+        leftBackMotor.setTargetPosition(lB);
+        rightFrontMotor.setTargetPosition(rF);
+        leftFrontMotor.setTargetPosition(lF);
+
+        rightBackMotor.setPower(power);
+        leftBackMotor.setPower(power);
+        rightFrontMotor.setPower(power);
+        leftFrontMotor.setPower(power);
+
+        rightBackMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftBackMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightFrontMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftFrontMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        // Blocking While Loop: doesn't break until all 4 motors have stopped moving
+        // Sets power of motors to 0 after the loop breaks
+        while (opModeIsActive() && (rightBackMotor.isBusy() || leftBackMotor.isBusy() || rightFrontMotor.isBusy() || leftFrontMotor.isBusy())) {
+        }
+
+        rightBackMotor.setPower(0);
+        leftBackMotor.setPower(0);
+        rightFrontMotor.setPower(0);
+        leftFrontMotor.setPower(0);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/pipelines/README.md
@@ -0,0 +1,5 @@
+To change the color threshold, go to line ~160 in the pipelines
+and raise or lower the value in the if statement
+
+Also need to change motor names in each auto, 
+and adjust the size of the boxes in the pieplines
